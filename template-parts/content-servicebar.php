3c654f65e1de0b7ce5df4c81d661d9b5f079aa05
Vipin Kumar Dinkar <vkd@HackerPros.local>
2020-11-04 17:11:11 +0530

    update customiser for servicebar

--- template-parts/content-servicebar.php
+++ template-parts/content-servicebar.php
@@ -1,13 +1,35 @@
+<?php
+/**
+ * The template for displaying the servicebar.
+ *
+ * @package Designfly
+ */
 
-<h1> hello from servicebar </h1>
+?>
 <div class="servicebar">
-<div class="service"> <div class="service__logo"><img src="" alt=""></div>
-<div class="service__body">
-	<h3><?php echo wp_kses_post( get_theme_mod( 'designfly-service-a-title' ) ); ?></h3>
-<p><?php echo wp_kses_post( get_theme_mod( 'designfly-service-a-body' ) ); ?></p>
-</div> </div>
+	<div class="service">
+	<div class="service__logo"><img src="" alt=""></div>
+		<div class="service__body">
+				<h3><?php echo wp_kses_post( get_theme_mod( 'designfly-service-a-title' ) ); ?></h3>
+				<p><?php echo wp_kses_post( get_theme_mod( 'designfly-service-a-body' ) ); ?></p>
+	</div> 
+	</div>
+
+	<div class="service">
+	<div class="service__logo"><img src="" alt=""></div>
+		<div class="service__body">
+				<h3><?php echo wp_kses_post( get_theme_mod( 'designfly-service-b-title' ) ); ?></h3>
+				<p><?php echo wp_kses_post( get_theme_mod( 'designfly-service-b-body' ) ); ?></p>
+	</div> 
+	</div>
+
+	<div class="service">
+	<div class="service__logo"><img src="" alt=""></div>
+		<div class="service__body">
+				<h3><?php echo wp_kses_post( get_theme_mod( 'designfly-service-c-title' ) ); ?></h3>
+				<p><?php echo wp_kses_post( get_theme_mod( 'designfly-service-c-body' ) ); ?></p>
+	</div> 
+	</div>
 
 
-<div class="service"> <div class="service__logo"><img src="" alt=""></div> <div class="service__body"><p></p></div> </div>
-<div class="service"> <div class="service__logo"><img src="" alt=""></div> <div class="service__body"><p></p></div> </div>
 </div>
