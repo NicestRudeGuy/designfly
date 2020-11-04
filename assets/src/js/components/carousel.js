121ceda7bb17e9a0b92235ac8c81311a80ad2a71
Vipin Kumar Dinkar <vipinkumard365@gmail.com>
2020-11-04 18:55:16 +0530

    add carousel js file

---
+++ assets/src/js/components/carousel.js
@@ -0,0 +1,37 @@
+/**
+ * Carousel JS.
+ *
+ */
+var slideIndex = 1;
+var homeURL = document.location.origin.concat ( '/' );
+var defaultLink = document.location.origin.concat ( '/#' );
+function showDivs( n ) {
+	var i;
+	var posts = document.getElementsByClassName( 'carousel__slides' );
+	if ( n > posts.length ) {
+		slideIndex = 1;
+	};
+	if ( 1 > n ) {
+		slideIndex = posts.length;
+	}
+	for ( i = 0; i < posts.length; i++ ) {
+		posts[i].style.display = 'none';
+	}
+	posts[slideIndex - 1].style.display = 'block';
+};
+
+if ( homeURL === document.location.href || defaultLink === document.location.href ) {
+	document.getElementById( 'carousel__button--left' ).addEventListener( 'click', function() {
+		slideIndex += 1;
+		showDivs( slideIndex );
+	}
+	);
+
+	document.getElementById( 'carousel__button--right' ).addEventListener( 'click', function() {
+		slideIndex -= 1;
+		showDivs( slideIndex );
+	}
+	);
+
+	showDivs( slideIndex );
+}
